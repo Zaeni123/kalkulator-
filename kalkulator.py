28ad13f15785b988b01b6529d00e52691494312d
Zendot <86873865+Zaeni123@users.noreply.github.com>
2021-08-22 11:39:14 +0700

    Add files via upload

---
+++ kalkulator.py
@@ -0,0 +1,33 @@
+# JANGAN DI RECODE!!
+# NANTI ERROR :V
+
+import sys
+import os
+import time
+
+os.system ("clear")
+
+banner='''
+               WELCOME
+-----------------------------------------
+[+] Github : https://github.com/zaeni123 |
+[+] Jangan lupa follow yah...^^          |
+-----------------------------------------
+
+ ____________________
+|    CARA PAKAI !!   |
+|                    |
+|Penambahan  = ( + ) |
+|Pengurangan = ( - ) |
+|Perkalian   = ( x ) |
+|Pembagian   = ( \ ) |
+|____________________|
+
+Keluar dari kalkulator = exit()  atau  quit()
+
+Silahlkan dicoba :)
+
+'''
+print(banner)
+
+os.system("python")
